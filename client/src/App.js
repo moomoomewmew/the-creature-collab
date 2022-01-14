aa67ba2075b47667ac5afdf4d770c0d073f7b8f0
Randall Rouse <randall.g.rouse@gmail.com>
2022-01-14 15:41:27 -0500

    added imports to app.js/ wrapped <browserRouter>

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
+import { useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router'
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import Login from './pages/login'
 
 function App() {
 
   return (
     <div className="App">
-    
+    <Login/>
 
       <Routes>
-        <Route path='/' element={<LandingPage />} />
+        {/* <Route path='/' element={<LandingPage />} />
         <Route path='/login' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/about' element={<About />} />
-        <Route path='/safety' element={<Safety />} />
+        <Route path='/safety' element={<Safety />} /> */}
       </Routes>
 
     </div>
