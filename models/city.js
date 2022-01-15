3f94911a56979cdc6e6f0582744d5f48981c6092
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-01-14 19:46:43 -0800

    added cityName to City model attributes. Please double check this is needed with the group

--- models/city.js
+++ models/city.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
   }
   City.init(
     {
+      cityName: DataTypes.STRING,
       events: DataTypes.ARRAY,
       users: {
         type: DataTypes.ARRAY(DataTypes.INTEGER),
