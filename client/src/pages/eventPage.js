055af2340939ecc6d4d36b9e76e8bb32a079e465
Katie Vermazani <katievermazani@tuta.io>
2022-01-16 16:51:56 -0700

    positioned the welcome box on landing page

--- client/src/pages/eventPage.js
+++ client/src/pages/eventPage.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import EventForm from '../components/EventForm';
 
-export default class Events extends Component {
-  render() {
-    return (
-      <div>
-        <EventForm />
-        <h2>{this.props.name}</h2>
-        <h3>{this.props.city}</h3>
-        <h4>{this.props.date}</h4>
-        <h4>{this.props.time}</h4>
-      </div>
-    );
-  }
+export default function Events() {
+
+  return (
+    <div>
+      <EventForm />
+      <h2>{this.props.name}</h2>
+      <h3>{this.props.city}</h3>
+      <h4>{this.props.date}</h4>
+      <h4>{this.props.time}</h4>
+    </div>
+  );
+
 }
--- client/src/pages/landingpage.js
+++ client/src/pages/landingpage.js
@@ -8,7 +8,9 @@ export default function Landingpage() {
                 <h1>Creature Collab</h1>
                 <p>Login as your character and find video game, board game, LARP, cosplay, and other fun events near you! You can also host an event and get connected to the perfect characters to join your campaign. </p>
             </div>
-            <img className='pub-pic' src='https://img.search.brave.com/8QLfDeGXo2RFzU6FcMuUqRVHPqC_vyvFeusCuTLT8S8/fit/1200/1079/ce/1/aHR0cDovL2RuZHNw/ZWFrLmNvbS93cC1j/b250ZW50L3VwbG9h/ZHMvMjAxOS8wMy8x/OTIweDEwNzktNDUx/NTE4NC1wb2ludGVk/LWVhcnMtZmFudGFz/eS1hcnQtdGF2ZXJu/LWNhbmRsZXMuanBn'></img>
+            <div>
+                <img className='pub-pic' src='https://img.search.brave.com/8QLfDeGXo2RFzU6FcMuUqRVHPqC_vyvFeusCuTLT8S8/fit/1200/1079/ce/1/aHR0cDovL2RuZHNw/ZWFrLmNvbS93cC1j/b250ZW50L3VwbG9h/ZHMvMjAxOS8wMy8x/OTIweDEwNzktNDUx/NTE4NC1wb2ludGVk/LWVhcnMtZmFudGFz/eS1hcnQtdGF2ZXJu/LWNhbmRsZXMuanBn'></img>
+            </div>
         </div >
 
     )
