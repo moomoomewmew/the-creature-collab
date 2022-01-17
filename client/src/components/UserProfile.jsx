5627324aa7d603bd74925eda75f46ea3e16d9c60
Randall Rouse <randall.g.rouse@gmail.com>
2022-01-17 14:03:05 -0500

    should have been working in profiles

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -23,11 +23,11 @@ const getDetails = async (user) => {
  
 
 return (
-<div >
+<div className="profile-layout-grid">
+    < img className = "profilePicture" src = {userDetails.profilePic} alt={userDetails.name}/>
     <h1> {userDetails.userName} </h1>
     <div className= "details">
-        <h3> Name: {userDetails.userName}</h3>
-        < img className = "profilePicture" src = {userDetails.image} alt={userDetails.name}/>
+        <h3> alias: {userDetails.characterName}</h3>
     </div>   
 </div>
 )
