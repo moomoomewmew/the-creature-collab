9d52cfc3fbcff249c9796b48706000465462b439
Randall Rouse <randall.g.rouse@gmail.com>
2022-01-18 16:33:50 -0500

    got auth working

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,24 +1,23 @@
-// import { redirect } from 'express/lib/response'
-// import React from 'react'
-// import { Navigate, Route } from 'react-router-dom'
+import React from 'react'
+import { Navigate, Route } from 'react-router-dom'
 
 
-// export default function ProtectedRoute({
-//     user,
-//     authenticated,
-//     element: Element,
-//     ...rest
-//   }) {
-//   return (
-//     <Route
-//       {...rest}
-//       render={
-//         user && authenticated ? (
-//           <Element {...props} /> // Render our chosen component if a user exists and they are authenticated
-//         ) : (
-//           <Navigate to="/login" /> // Otherwise, use the Redirect component to return the user to the sign in screen
-//         )
-//       }
-//     />
-//   )
-//     }
+export default function ProtectedRoute({
+    user,
+    authenticated,
+    component: Component,
+    ...rest
+  }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        user && authenticated ? (
+          <Component {...props} /> // Render our chosen component if a user exists and they are authenticated
+        ) : (
+          <Navigate to="/signin" /> // Otherwise, use the Redirect component to return the user to the sign in screen
+        )
+      }
+    />
+  )
+    }
