50c7f06f3352d66c8507269fa661c4ffceba9610
Katie Vermazani <katievermazani@tuta.io>
2022-01-18 13:08:41 -0700

    continuing  attempt 4

--- server.js
+++ server.js
@@ -14,4 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => res.json({ message: 'Server Works' }));
 app.use('/api', AppRouter);
 app.use('/auth', AuthRouter);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+    })
+}
+
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
