e7b0160448df2027acf07cbfa30f114eac46dfd3
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-01-19 15:35:57 -0800

    updated globals base url and began process of mapping event info

--- client/src/globals/index.js
+++ client/src/globals/index.js
@@ -1,3 +1,4 @@
-process.env.NODE_ENV === 'production'
+export const BASE_URL =
+  process.env.NODE_ENV === 'production'
     ? `${window.location.origin}/api`
-    : 'http://localhost:3001/api'
+    : 'http://localhost:3001/api';
--- client/src/pages/eventPage.js
+++ client/src/pages/eventPage.js
@@ -1,11 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import EventForm from '../components/EventForm';
+import axios from 'axios';
+import { BASE_URL } from '../globals/index';
+import EventCard from '../components/EventCard';
+import '../styles/events.css';
 
 export default function Events(props) {
+  const [events, setEvents] = useState([]);
+
+  const getEvents = async () => {
+    const res = await axios.get(`${BASE_URL}/events`);
+    setEvents(res.data);
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
 
   return (
-    <div>
-      <EventForm />
+    <div className="event-page">
+      <div className="event-form">
+        <EventForm />
+      </div>
+      <div className="events">
+        <h1>Events</h1>
+        {events.map((event) => {
+          return (
+            <div key={event.id}>
+              <EventCard event={event} />
+            </div>
+          );
+        })}
+      </div>
       <h2>{props.name}</h2>
       <h3>{props.city}</h3>
       <h4>{props.date}</h4>
@@ -14,7 +40,6 @@ export default function Events(props) {
       <h4>{props.state}</h4>
       <h4>{props.description}</h4>
       <h4>{props.picture}</h4>
-
     </div>
   );
 }
