dec21943d7657a8b3d5a988b5a6c406e7ef43220
Katie Vermazani <katievermazani@tuta.io>
2022-01-19 17:31:18 -0700

    logout button is functional

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 
 const Navbar = (props) => {
 
-    const user = props.user
     const authenticated = props.authenticated
 
     if (authenticated) {
@@ -12,7 +11,7 @@ const Navbar = (props) => {
                 <Link className="nav" to="/">Creature Collab</Link>
                 <div className="login_and_CA">
                     <Link className="dashboard-button" to="/dashboard">Dashboard</Link>
-                    <Link className="logout-button" to="/">Logout</Link>
+                    <Link className="logout-button" onClick={props.handleLogOut} to="/">Logout</Link>
                     <Link className="profile-button" to='/profiles'>Members</Link>
 
                 </div>
