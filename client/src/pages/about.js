eee1f0c3d150bc8bf0fe967744760d72d931cdbc
shelbylin13 <shelbylin13@gmail.com>
2022-01-19 21:19:44 -0600

    added content to about page

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 export default function About() {
-    return (
-        <div>
-            <h1>About Creature Collab</h1>
-            <p>It all began one fateful day when four coding warriors croseed paths at General Assembly and combined foces to create this service you now know as Creature Collab.</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>About Creature Collab</h1>
+      <p>
+        It all began one fateful day when four coding warriors crossed paths at
+        General Assembly and combined foces to create this service you now know
+        as Creature Collab.
+      </p>
+      <p>
+        In these times of isolation, the hero's journey can feel exccedingly
+        lonesome. Our collective brain child was born from the desire to find
+        fellow fantasy enthusiasts and forge friendships based on shared
+        interests in all things fictional and fun. Our cast of characters
+        include:
+        <li className="Katie">Katie, our fearless Gatekeeper</li>
+        <li className="Randall">Randall, our Creative Conjurer</li>
+        <li className="Benjamin">Benjamin, our Human Room of Requirement</li>
+        <li className="Charlie">And Charlie, our Developer Druid</li>
+      </p>
+    </div>
+  );
 }
