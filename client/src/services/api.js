6b48194d8646d0de472ca54ce50caccc88bdc8d0
Katie Vermazani <katievermazani@tuta.io>
2022-01-20 08:31:23 -0700

    added conditional to api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001'
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://the-creature-collab.herokuapp.com/' : 'http://localhost:3001'
 const Client = Axios.create({ baseURL: BASE_URL })
 // Intercepts every request axios makes
 Client.interceptors.request.use(
